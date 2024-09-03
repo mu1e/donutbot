f50b2828576f436820c63ac6c5100b71de4b80c5
chowt <chowt@DESKTOP-EIV85H0>
2024-09-02 20:27:28 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace donutbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.phonenumberTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.zipcodeTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(287, 155);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(233, 20);
+            this.emailTxt.TabIndex = 1;
+            this.emailTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(287, 195);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(233, 20);
+            this.passwordTxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KrispyKreme Rewards Account generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "enter desired info and click create to make KrispyKreme rewards account";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "email:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(226, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "first name:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(287, 239);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(233, 20);
+            this.nameTxt.TabIndex = 7;
+            this.nameTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "phone number (xxxxxxxxxx numbers only)";
+            // 
+            // phonenumberTxt
+            // 
+            this.phonenumberTxt.Location = new System.Drawing.Point(287, 277);
+            this.phonenumberTxt.Name = "phonenumberTxt";
+            this.phonenumberTxt.Size = new System.Drawing.Size(233, 20);
+            this.phonenumberTxt.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "zipcode:";
+            // 
+            // zipcodeTxt
+            // 
+            this.zipcodeTxt.Location = new System.Drawing.Point(287, 316);
+            this.zipcodeTxt.Name = "zipcodeTxt";
+            this.zipcodeTxt.Size = new System.Drawing.Size(233, 20);
+            this.zipcodeTxt.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.zipcodeTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.phonenumberTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.emailTxt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox phonenumberTxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox zipcodeTxt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace donutbot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string email = "1";
+            string password = "2";
+            string name = "3";
+            string zip = "4";
+            string phonenumber = "5";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = emailTxt.Text;
+            string password = passwordTxt.Text;
+            string name = nameTxt.Text;
+            string lastname = name + "lastname";
+            string phone = phonenumberTxt.Text;
+            var driver = new ChromeDriver();
+            string url = "https://www.krispykreme.com/account/create-account";
+            driver.Navigate().GoToUrl(url);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            //email input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[6]/div/input")));
+            var emailInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[6]/div/input"));
+            emailInput.SendKeys(email);
+            //first name input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[1]/div/input")));
+            var fnInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[1]/div/input"));
+            fnInput.SendKeys(name);
+            //last name input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[2]/div/input")));
+            var lnInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[2]/div/input"));
+            lnInput.SendKeys(lastname);
+            //phone input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[5]/div/span/span[1]/input")));
+            var phoneInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[5]/div/span/span[1]/input"));
+            phoneInput.SendKeys(phone);
+            //pw input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[7]/div/input")));
+            var pwInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[7]/div/input"));
+            pwInput.SendKeys(password);
+            //pwc input
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[8]/div/input")));
+            var pwcInput = driver.FindElement(By.XPath("/html/body/form/div[3]/div/div/main/section/div[1]/div[1]/div[8]/div/input"));
+            pwcInput.SendKeys(password);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
